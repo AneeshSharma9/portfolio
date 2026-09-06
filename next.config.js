7169caf313fdf68c38b475c4bb202d8214b98b68
Aneesh Sharma <ansh993@gmail.com>
2026-09-06 14:45:27 -0400

    fix: changed favicon paths since they weren't accessible from before

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  basePath,
 }
 
 module.exports = nextConfig
